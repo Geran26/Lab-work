3e10ef816cc75189f4c9936b75fc25a0de864862
Geran Dunlop <geran127@gmail.com>
2019-05-02 23:37:18 +0000

    Lab Week 8

--- lab.c
+++ lab.c
@@ -1,6 +1,20 @@
-#inlcude<stdio.h>
+#include <stdio.h>
+
+// Lab Task Week 8 - calculating the mean of 3 numbers and zero's the other two
+
+void mean(int *x, int *y, int *z);
 
 int main() {
-  printf("Hello, World");
-  return 0;
+    int a = 1, b = 2, c = 3;
+    printf("%d %d %d\n", a, b, c);
+    mean(&a, &b, &c);
+    printf("%d %d %d\n", a, b, c);
+    return 0;
+}
+
+void mean(int *x, int *y, int *z) {
+    *x = (int)(*x + *y + *z)/3.0;
+    *y = 0;
+    *z = 0;
+    return;
 }
