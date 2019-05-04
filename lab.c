b35a407ed85639840da0f430fbf83692f0fe0989
Geran Dunlop <geran127@gmail.com>
2019-05-04 07:56:32 +0000

    Lab Work Week 8 (prep for Tuesday)

--- lab.c
+++ lab.c
@@ -1,20 +1,34 @@
 #include <stdio.h>
 
-// Lab Task Week 8 - calculating the mean of 3 numbers and zero's the other two
-
-void mean(int *x, int *y, int *z);
-
 int main() {
-    int a = 1, b = 2, c = 3;
-    printf("%d %d %d\n", a, b, c);
-    mean(&a, &b, &c);
-    printf("%d %d %d\n", a, b, c);
+    FILE *input;
+    input = fopen("test.txt", "r");
+    if(input == NULL) {
+        printf("cannot open file");
+        return 0;
+    }
+    int arrayData[3][3];
+    int row = 0, column = 0;
+    while(!feof(input)) {
+        for(row = 0; row<3; row++){
+            for(column = 0; column<3; column++){
+                    fscanf(input, "%d", &arrayData[row][column]);
+            }
+        }
+    }
+    int maximum = arrayData[0][0]; //records the ongoing maximum number
+    int location_row = 0, location_column = 0;
+    for(row = 0; row < 3; row++){ //For all elements of the string (This function only needs i to be less than 26 as there is only26 possible rotations)
+        for(column = 0; column < 3; column++) {
+            if(arrayData[row][column] > maximum) {
+                maximum = arrayData[row][column];
+                location_row = row;
+                location_column = column;
+            }      
+        }
+    }
+    printf("Maximum element is arrayData[%d][%d] = %d\n", location_row, location_column, maximum);
+    fclose(input);
     return 0;
 }
-
-void mean(int *x, int *y, int *z) {
-    *x = (int)(*x + *y + *z)/3.0;
-    *y = 0;
-    *z = 0;
-    return;
-}
+    
